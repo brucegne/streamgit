d46510cb826b6af040b5d2231c96babc03d2cfa9
Bruce Gordon <brucegne@gmail.com>
2023-05-11 12:18:32 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
+pholder = st.sidebar.empty()
+with pholder.container():
+  uname = st.text_input("Enter User Name")
+  upass = st.text_input("Enter Password")
+  
 lcol, rcol = st.columns([3,1])
 
 lcol.header("This is on the left")
