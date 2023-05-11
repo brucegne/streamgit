727978ae7ab541dd22d051ad31626727bcb34334
Bruce Gordon <brucegne@gmail.com>
2023-05-11 22:37:31 +0000

    login

--- Home.py
+++ Home.py
@@ -3,22 +3,30 @@ from st_pages import Page, add_page_title, show_pages
 
 add_page_title("Welcome to my site")
 
-show_pages(
-  [
-    Page("Home.py","Home",""),
-    Page("page/contacts.py","Contacts","🏡"),
-    Page("page/photos.py","Photo Library","📸"),
-  ]
-)
+def bldmenu():
+  show_pages(
+    [
+      Page("Home.py","Home",""),
+      Page("page/contacts.py","Contacts","🏡"),
+      Page("page/photos.py","Photo Library","📸"),
+    ]
+  )
 
-pholder = st.sidebar.empty()
-with pholder.container():
-  uname = st.text_input("Enter User Name")
-  upass = st.text_input("Enter Password")
-  login = st.button("Login")
-  if login:
-    pholder.write("")
-  
+def dispLog():
+  pholder = st.sidebar.empty()
+  with pholder.container():
+    uname = st.text_input("Enter User Name")
+    upass = st.text_input("Enter Password")
+    login = st.button("Login")
+    if login:
+      st.session_state['logStatus'] = 'Yes'
+      pholder.write("")
+
+if 'logStatus' not in st.session_state:
+  dispLog()
+else:
+  bldmenu()   
+
 lcol, rcol = st.columns([3,1])
 
 lcol.header("This is on the left")
