c4a0cded0ec47f99ae6ea95f18bd1d035764d803
Bruce Gordon <brucegne@gmail.com>
2023-05-13 01:30:49 +0000

    spacing

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ add_page_title()
 def bldMenu():
   show_pages(
     [
-      Page("Home.py","Home",":us:"),
+      Page("Home.py","Home","🇺🇸"),
       Page("contacts.py","Contacts","🏡"),
       Page("photos.py","Photo Library","📸"),
     ]
