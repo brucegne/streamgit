e5d7d289433196ec1d7bb54ec0f0d2bf8737c140
Bruce Gordon <brucegne@gmail.com>
2023-06-08 05:22:59 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -27,5 +27,5 @@ with lcol:
     upasswd=st.text_input("Password", max_chars=15, key="upass", type="password")
     logbtn=st.form_submit_button("Login")
     if logbtn:
-        switch_page("Contacts")
+        switch_page("DetaZips")
         
