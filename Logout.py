6671453909dab72bcf6799eb8332010302ec6d7b
Bruce Gordon <brucegne@gmail.com>
2023-06-08 19:43:02 +0000

    finally

--- Logout.py
+++ Logout.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from st_pages import Page, add_page_title, show_pages
 from streamlit_extras.switch_page_button import switch_page
-try:
-    del st.session_state['logUser']
-except:
-    x=12
+
+for key in st.session_state.keys():
+    del st.session_state[key]
+
 switch_page('Home')
 
