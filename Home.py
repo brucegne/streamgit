797a97ebf42ee12e880a64535c427c10c2a28058
Bruce Gordon <brucegne@gmail.com>
2023-06-09 20:24:41 +0000

    debuging

--- Home.py
+++ Home.py
@@ -20,33 +20,36 @@ def bldMenu():
 bldMenu()
 
 hdr = st.empty()
-logit = st.sidebar.empty()
 
 # Connect to Deta Base with your Data Key
 
-lcol, rcol = st.columns([1,1])
+if "full_name" in st.session_state:
+   st.header(st.session_state['full_name'])
+else:
+    lcol, rcol = st.columns([1,1])
 
-with lcol:
-  st.subheader("Please Login")
-  with st.form("lform", clear_on_submit=True):
-    usrname=st.text_input("User Name", max_chars=20, key="uname")
-    upasswd=st.text_input("Password", max_chars=20, key="upass", type="password")
-    logbtn=st.form_submit_button("Login")
-    if logbtn:
-        qryString = {}
-        deta = Deta("b0fvbhznajp_skZPFvqP1e1dV6eK5BHkuDm72uM6FKf4")
-        db = deta.Base("UserBase")
-        qryString["login_name"] = usrname.strip()
-        recs = db.fetch(qryString).items
-        if len(recs) > 0:
-            file_pass = recs[0]['passwd']
-            if file_pass == upasswd:
-                logit.write("Welcome " + recs[0]["full_name"])
-                st.session_state['logUser'] = usrname
-                switch_page("Zipcode Lookup")
+    with lcol:
+      st.subheader("Please Login")
+      with st.form("lform", clear_on_submit=True):
+        usrname=st.text_input("User Name", max_chars=20, key="uname")
+        upasswd=st.text_input("Password", max_chars=20, key="upass", type="password")
+        logbtn=st.form_submit_button("Login")
+        if logbtn:
+            qryString = {}
+            deta = Deta("b0fvbhznajp_skZPFvqP1e1dV6eK5BHkuDm72uM6FKf4")
+            db = deta.Base("UserBase")
+            qryString["login_name"] = usrname.strip()
+            recs = db.fetch(qryString).items
+            if len(recs) > 0:
+                file_pass = recs[0]['passwd']
+                if file_pass == upasswd:
+                    logit.write("Welcome " + recs[0]["full_name"])
+                    st.session_state['logUser'] = usrname
+                    st.session_state['full_name'] = recs[0]['full_name']
+                    switch_page("Zipcode Lookup")
+                else:
+                  hdr.write("Invalid password entered")
             else:
-               hdr.write("Invalid password entered")
-        else:
-           hdr.write("Invalid user name or password")
+              hdr.write("Invalid user name or password")
 
         
